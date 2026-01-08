272cfb772c388ffd749e6ee1369d0359c327ddc3
Halleey <aparecidohallyson@gmail.com>
2026-01-08 12:16:06 -0300

    fixed static context

--- src/memory_manager/ownership/OwnershipAnalyzer.java
+++ src/memory_manager/ownership/OwnershipAnalyzer.java
@@ -1,24 +1,12 @@
 package memory_manager.ownership;
 
 import ast.ASTNode;
-import ast.exceptions.ReturnNode;
-import ast.functions.FunctionCallNode;
-import ast.lists.ListAddNode;
-import ast.structs.StructFieldAccessNode;
-import ast.structs.StructInstanceNode;
-import ast.structs.StructUpdateNode;
-import ast.variables.AssignmentNode;
-import ast.variables.VariableDeclarationNode;
-import ast.variables.VariableNode;
-import memory_manager.ownership.enums.OwnerShipAction;
-import memory_manager.ownership.enums.OwnershipState;
 import memory_manager.ownership.functions.FunctionCallHandler;
 import memory_manager.ownership.functions.ReturnHandler;
 import memory_manager.ownership.graphs.OwnershipGraph;
 import memory_manager.ownership.lists.ListAddHandler;
 import memory_manager.ownership.structs.InlineUpdateHandler;
 import memory_manager.ownership.structs.StructFieldHandler;
-import memory_manager.ownership.utils.OwnershipUtils;
 import memory_manager.ownership.variables.AssignmentHandler;
 import memory_manager.ownership.variables.DeclarationHandler;
 import memory_manager.ownership.variables.NodeHandler;

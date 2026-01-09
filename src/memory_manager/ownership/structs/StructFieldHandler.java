69a1a4cc6d8d58f9d1479bf41bb0892cedb86e7b
Halleey <aparecidohallyson@gmail.com>
2026-01-09 18:18:14 -0300

    fixed bind structfield acess

--- src/memory_manager/ownership/structs/StructFieldHandler.java
+++ src/memory_manager/ownership/structs/StructFieldHandler.java
@@ -14,6 +14,8 @@ import memory_manager.ownership.variables.NodeHandler;
 
 import java.util.List;
 import java.util.Map;
+
+
 public class StructFieldHandler implements NodeHandler<StructFieldAccessNode> {
 
     @Override

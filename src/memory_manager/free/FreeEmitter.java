f3a816e3dbafca5ad825f961df71fd4e35493516
Halleey <aparecidohallyson@gmail.com>
2026-01-11 08:26:07 -0300

    refactoring free emitter

--- src/memory_manager/free/FreeEmitter.java
+++ src/memory_manager/free/FreeEmitter.java
@@ -1,24 +1,30 @@
 package memory_manager.free;
 
+import low.TempManager;
 import low.functions.TypeMapper;
 import low.module.LLVisitorMain;
 import memory_manager.ownership.graphs.OwnershipNode;
+
 public class FreeEmitter {
 
     private final LLVisitorMain visitor;
     private final TypeMapper typeMapper;
+    private final TempManager temps;
 
-    public FreeEmitter(LLVisitorMain visitor) {
+    public FreeEmitter(LLVisitorMain visitor, TempManager temps) {
         this.visitor = visitor;
         this.typeMapper = new TypeMapper();
+        this.temps  = temps;
     }
 
     public String emit(FreeNode freeNode) {
         OwnershipNode root = freeNode.getRoot();
+        StringBuilder llvm = new StringBuilder();
 
-        // Nome da variável agora vem do Symbol
+        // nome real da variável LLVM (%lista, %pessoas, etc)
         String varName = root.getSymbol().getName();
 
+        // só listas têm free aqui
         String elementType = visitor.getListElementType(varName);
         if (elementType == null) {
             return "";
@@ -27,11 +33,27 @@ public class FreeEmitter {
         String llvmType = typeMapper.toLLVM("List<" + elementType + ">");
         String freeFunc = typeMapper.freeFunctionForElement(elementType);
 
-        return String.format(
-                "  %%tmpFree_%s = load %s, %s* %%%s\n" +
-                        "  call void %s(%s %%tmpFree_%s)",
-                varName, llvmType, llvmType, varName,
-                freeFunc, llvmType, varName
-        );
+        // temp para carregar a lista
+        String tmpList = temps.newTempWithPrefix("free");
+
+        llvm.append("  ")
+                .append(tmpList)
+                .append(" = load ")
+                .append(llvmType)
+                .append(", ")
+                .append(llvmType)
+                .append("* %")
+                .append(varName)
+                .append("\n");
+
+        llvm.append("  call void ")
+                .append(freeFunc)
+                .append("(")
+                .append(llvmType)
+                .append(" ")
+                .append(tmpList)
+                .append(")\n");
+
+        return llvm.toString();
     }
 }

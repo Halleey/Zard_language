d5058a087d97eecc1f66a9ce34ffa903cc121aee
Halleey <aparecidohallyson@gmail.com>
2026-02-28 14:50:41 -0300

    removed 'fix' in llvm generation

--- src/low/variables/exps/AllocaEmitter.java
+++ src/low/variables/exps/AllocaEmitter.java
@@ -1,6 +1,8 @@
 package low.variables.exps;
 
+
 import ast.variables.VariableDeclarationNode;
+
 import low.TempManager;
 import low.functions.TypeMapper;
 import low.main.TypeInfos;
@@ -9,12 +11,13 @@ import low.variables.VariableEmitter;
 
 import java.util.Map;
 
+
 public class AllocaEmitter {
 
     private final Map<String, TypeInfos> varTypes;
     private final TempManager temps;
     private final LLVisitorMain visitor;
-    private final VariableEmitter varEmitter;
+    private final VariableEmitter varEmitter; // precisa para pegar o scope ID
 
     public AllocaEmitter(Map<String, TypeInfos> varTypes,
                          TempManager temps,
@@ -30,14 +33,20 @@ public class AllocaEmitter {
         return new TypeMapper().toLLVM(sourceType);
     }
 
-
     private String newPtr(String varName) {
-        int id = temps.nextVarId();  // contador global de variáveis
-        String ptr = "%" + varName + "_v" + id;
+        // Se estamos no escopo global (escopo raiz = 0), não adiciona sufixo
+        int currentScopeId = varEmitter.getScopeId();
+        String ptr;
+        if (currentScopeId == 0) {
+            ptr = "%" + varName; // nome “limpo” para global
+        } else {
+            ptr = "%" + varName + "_" + currentScopeId; // nome único para escopos internos
+        }
 
         varEmitter.registerVarPtr(varName, ptr);
         return ptr;
     }
+
 
     public String emit(VariableDeclarationNode node) {
         String srcType = node.getType();
@@ -54,14 +63,12 @@ public class AllocaEmitter {
                 return "  " + ptr + " = alloca %String*\n"
                         + ";;VAL:" + ptr + ";;TYPE:%String*\n";
             }
-
             case "char" -> {
                 llvmType = "i8";
                 varTypes.put(varName, new TypeInfos(srcType, llvmType, null));
                 return "  " + ptr + " = alloca i8\n"
                         + ";;VAL:" + ptr + ";;TYPE:i8\n";
             }
-
             case "List<int>" -> {
                 llvmType = "%struct.ArrayListInt*";
                 elemType = "int";
@@ -69,7 +76,6 @@ public class AllocaEmitter {
                 return "  " + ptr + " = alloca %struct.ArrayListInt*\n"
                         + ";;VAL:" + ptr + ";;TYPE:%struct.ArrayListInt*\n";
             }
-
             case "List<double>" -> {
                 llvmType = "%struct.ArrayListDouble*";
                 elemType = "double";
@@ -77,7 +83,6 @@ public class AllocaEmitter {
                 return "  " + ptr + " = alloca %struct.ArrayListDouble*\n"
                         + ";;VAL:" + ptr + ";;TYPE:%struct.ArrayListDouble*\n";
             }
-
             case "List<boolean>" -> {
                 llvmType = "%struct.ArrayListBool*";
                 elemType = "boolean";
@@ -85,7 +90,6 @@ public class AllocaEmitter {
                 return "  " + ptr + " = alloca %struct.ArrayListBool*\n"
                         + ";;VAL:" + ptr + ";;TYPE:%struct.ArrayListBool*\n";
             }
-
             default -> {
                 if (srcType.startsWith("List<")) {
                     String inner = srcType.substring(5, srcType.length() - 1).trim();
@@ -95,7 +99,6 @@ public class AllocaEmitter {
                     return "  " + ptr + " = alloca %ArrayList*\n"
                             + ";;VAL:" + ptr + ";;TYPE:%ArrayList*\n";
                 }
-
                 llvmType = mapLLVMType(srcType);
                 varTypes.put(varName, new TypeInfos(srcType, llvmType, null));
                 return "  " + ptr + " = alloca " + llvmType + "\n"
